a23569813e1bf493968022336cabe2cd36d35c7e
Ms. Nullus <70487766+msNullus@users.noreply.github.com>
2022-08-27 21:09:32 -0300

    add new changes in database

--- database/projeto_cadmo_sa.sql
+++ database/projeto_cadmo_sa.sql
@@ -1,27 +1,32 @@
 -- criar banco projeto_cadmo_sa
 CREATE DATABASE `projeto_cadmo_sa`;
 
+-- selecionar o banco de dados
+USE `projeto_cadmo_sa`;
+
 -- criar tabelas
 CREATE TABLE `paciente` (
-	cod INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    telefone VARCHAR(15)
+	cod_paciente INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(100) NOT NULL,
+	telefone VARCHAR(15)
 );
-
 
 CREATE TABLE `dentista` (
 	crm INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    espec VARCHAR(100) NOT NULL,
-    valor FLOAT(9) NOT NULL
+	nome VARCHAR(100) NOT NULL,
+	espec VARCHAR(100) NOT NULL,
+	valor FLOAT(9) NOT NULL
 );
 
-
 CREATE TABLE `consulta` (
-	cod INT PRIMARY KEY AUTO_INCREMENT,
+    cod INT PRIMARY KEY AUTO_INCREMENT,
     crm INT,
+    cod_paciente INT,
     hora DATETIME NOT NULL,
     valor FLOAT(9) NOT NULL,
-    FOREIGN KEY (crm) REFERENCES dentista(crm)
+    
+    FOREIGN KEY (crm)
+        REFERENCES dentista (crm),
+    FOREIGN KEY (cod_paciente)
+        REFERENCES paciente (cod_paciente)
 );	
-
